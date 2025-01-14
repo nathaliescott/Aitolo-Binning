6f16b260923b60bb555250dd77b087c8ccad19ca
nathalie <nss2337@utexas.edu>
2025-01-14 11:55:58 -0600

    git config pull.rebase false

--- Aitolo_Binning.R
+++ Aitolo_Binning.R
@@ -1,7 +1,9 @@
 #FastQC Script#
+
 nohup time fastqc -t 15 *.fastq
 
 ---
+  
 #FastP Script#
   
 SAMPLE_R1_C11=/home/nss2337/Aitolo/rawreads/C11_S1_L004_R1_001.fastq
@@ -26,5 +28,3 @@ fastp --thread 16 -i $SAMPLE_R1_C14 -I $SAMPLE_R2_C14 -o $OUT_R1_C14 -O $OUT_R2_
 fastp --thread 16 -i $SAMPLE_R1_C15 -I $SAMPLE_R2_C15 -o $OUT_R1_C15 -O $OUT_R2_C15 -g -x --adapter_sequence=AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --adapter_sequence_r2=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT --qualified_quality_phred 33 --overrepresentation_analysis 5 --cut_right cut_right_window_size=5 cut_right_mean_quality=15 --length_required 50 &
 fastp --thread 16 -i $SAMPLE_R1_C16 -I $SAMPLE_R2_C16 -o $OUT_R1_C16 -O $OUT_R2_C16 -g -x --adapter_sequence=AGATCGGAAGAGCACACGTCTGAACTCCAGTCA --adapter_sequence_r2=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT --qualified_quality_phred 33 --overrepresentation_analysis 5 --cut_right cut_right_window_size=5 cut_right_mean_quality=15 --length_required 50 &
 echo "Fastp Done"
-
-
